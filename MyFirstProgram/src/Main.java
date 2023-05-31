74b5e2e24a84f56a890df6c3e52039981c122820
Colorfuls <bike.chanokchon@gmail.com>
2023-05-31 12:18:40 +0700

    add rendom

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -1,24 +1,9 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) {
-		double x;
-		double y;
-		double z;
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Enter size x: ");
-		x = scanner.nextDouble();
-		
-		System.out.print("Enter size y: ");
-		y = scanner.nextDouble();
-		
-		z = Math.sqrt((x * x) + (y * y));
-		
-		System.out.println("z is " + z);
-		
-		scanner.close();
-		
+		Random random = new Random();	
+		int x = random.nextInt(6);
+		System.out.println(x);
 	}
 }
